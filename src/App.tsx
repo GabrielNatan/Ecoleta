e6194e91b76c12a15af0d6c251c918c471aff425
Gabriel Natan <gabriel--natan@live.com>
2021-10-10 23:00:37 -0300

    configuring folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import {BrowserRouter as Router, Route,Switch} from "react-router-dom"
+import {Home} from "./Pages/Home"
+import {Cadastro} from "./Pages/Cadastro"
+import {Lista} from "./Pages/Lista"
+
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route exact={true} path="/" component={Home}/>
+        <Route exact={true} path="/" component={Cadastro}/>
+        <Route exact={true} path="/" component={Lista}/>
+      </Switch>
+    </Router>
   );
 }
 
