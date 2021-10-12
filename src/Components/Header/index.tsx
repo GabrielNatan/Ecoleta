8b9c87cbaccd9d2436feb5f966e8aa2c39dca432
Gabriel Natan <gabriel--natan@live.com>
2021-10-12 20:26:40 -0300

    add link header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,12 +1,16 @@
 import { Container } from "./style"
 import Logo from "../../Assets/Logo.png"
+import Voltar from "../../Assets/Voltar.png"
 import Enter from "../../Assets/Login.png"
-import { Link } from "react-router-dom"
+import { Link,useHistory } from "react-router-dom"
 export const Header = () => {
+    const {location} = useHistory();
+    console.log(location.pathname)
     return (
         <Container>
-            <img src={Logo} alt="logo" />
-            <Link to="/cadastro"><img src={Enter} alt="enter" /> Cadastre um ponto de coleta</Link>
+            <Link to="/"><img src={Logo} alt="logo" /></Link>
+            {location.pathname === "/" ?<Link to="/cadastro"><img src={Enter} alt="enter" /> Cadastre um ponto de coleta</Link>
+             :<Link to="/"><img src={Enter} alt="enter" />Voltar para home</Link>}
         </Container>
     )
 }
