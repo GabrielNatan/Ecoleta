3d52d4c7c08d9bee3c5cd39761db65f9bcab23b1
Gabriel Natan <gabriel--natan@live.com>
2021-10-12 15:48:39 -0300

    new route

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import {BrowserRouter as Router, Route,Switch} from "react-router-dom"
 import {Home} from "./Pages/Home"
 import {Cadastro} from "./Pages/Cadastro"
 import {Lista} from "./Pages/Lista"
-
+import {GlobalStyle} from "./styleGlobal"
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact={true} path="/" component={Home}/>
-        <Route exact={true} path="/" component={Cadastro}/>
-        <Route exact={true} path="/" component={Lista}/>
+        <Route exact={true} path="/cadastro" component={Cadastro}/>
+        <Route exact={true} path="/lista" component={Lista}/>
       </Switch>
+      <GlobalStyle/>
     </Router>
   );
 }
