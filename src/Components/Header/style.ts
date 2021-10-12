ac8abd994d8fc03259336a52b0257a4c6318a7f4
Gabriel Natan <gabriel--natan@live.com>
2021-10-12 15:49:19 -0300

    add header style

---
+++ src/Components/Header/style.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+
+        width: 80%;
+        margin: 0 auto;
+        padding: 55px 0;
+        display: flex;
+        justify-content: space-between;
+
+        a{
+            display: flex;
+            align-items: center;
+            text-decoration: none;
+            font-weight: bolder;
+            font-size: var(--secondary-title-font);
+            gap: 25px;
+
+        }
+    
+    
+`
