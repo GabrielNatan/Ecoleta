9e903d1c3ce3ea1f013728245d3bb141dc105bf8
Gabriel Natan <gabriel--natan@live.com>
2021-10-12 15:49:32 -0300

    add page Cadastro

--- src/Pages/Cadastro/index.tsx
+++ src/Pages/Cadastro/index.tsx
@@ -1,5 +1,57 @@
-export const Cadastro = ()=>{
-    return(
-        <div></div>
+import { Header } from "../../Components/Header"
+import { Container, Cont } from "./style"
+
+export const Cadastro = () => {
+    return (
+        <Container>
+            <Header />
+            <Cont>
+                <h1>Cadastro do ponto de coleta</h1>
+
+                <form>
+                    <h2>Dados da entidade</h2>
+                    <label>
+                        Nome da entidade
+                        <input type="text" />
+                    </label>
+                    <div className="cont-input">
+                        <label className="label-big">
+                            Endereço
+                            <input type="text" />
+                        </label>
+                        <label  className="numero">
+                            Número
+                            <input type="text" />
+                        </label>
+                    </div>
+                    <div className="cont-input">
+                        <label className="label-big">
+                            Cidade
+                            <input type="text" />
+                        </label>
+                        <label className="label-big">
+                            Estado
+                            <input type="text" />
+                        </label>
+                    </div>
+
+                    <div className="checkbox-cont">
+                        <div className="checkbox-header">
+                            <h2>Ítens de coleta</h2>
+                            <span>Selecione um ou mais ítens abaixo</span>
+                        </div>
+                        <div className="checkbox-cont-card">
+                            <label className="checkbox-card">
+                                <img src="" alt="icone"/>
+                                <input type="checkbox" name="categoria"/>
+                            </label>
+                        </div>
+                       
+                    </div>
+
+                    <button className="btn-form" type="submit">Cadastrar ponto de coleta</button>
+                </form>
+            </Cont>
+        </Container>
     )
 }
